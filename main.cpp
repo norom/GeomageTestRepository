e631077bbd1c5b1782c03f0f43e86e8777ed8e12
petr <norom@yandex.ru>
2015-09-01 23:41:36 +0300

    dot

--- main.cpp
+++ main.cpp
@@ -7,5 +7,5 @@ int main()
   std::string name;
   std::cout << "What is your name? ";
   getline (std::cin, name);
-  std::cout << "Hello, " << name << "!\n";
+  std::cout << "Hello. " << name << "!\n";
 }
